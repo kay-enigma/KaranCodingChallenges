8876070a18b3c2872374e5c4fa893d3f2ef114c2
Karan <Karan.malhotra@ucalgary.ca>
2023-04-09 16:50:13 -0600

    CC_Polymorphism
    - All adjustments made accordingly to the D2L instructions.
    - All test now pass; TripComponentTest, FightTest, ItineraryTest.

--- src/oosequence/Itinerary.java
+++ src/oosequence/Itinerary.java
@@ -1,67 +1,73 @@
+
 package oosequence;
 
 
 import java.util.ArrayList;
 
-import java.util.Date;
 
 
 public class Itinerary {
 	
-	private String flight_name;
-	private ArrayList<Flight> List_flights;
+	private String name;
+	private ArrayList<TripComponent> flightlist=new ArrayList<TripComponent>();
 	
-	public Itinerary(String StringName) {
-	   flight_name=StringName;
+	public Itinerary(String givenName) {
+		name=givenName;
+		flightlist=new ArrayList<TripComponent>();
 	}
 	
-	public String getName() {
-		return flight_name;
-	}
+	public String getName() {return name;}
 	
-	public ArrayList<Flight> getFlights() {
-		return List_flights;
-	}
+	public ArrayList<TripComponent> getTripComponents() {return flightlist;}
 	
-	public void addFlight(Flight added_flight) {
-		ArrayList<Flight> temp = new ArrayList<Flight>();
-		if(List_flights == null)	{	
-			temp.add(added_flight);
-			List_flights = temp;
+	public void addTripComponent(TripComponent trip) {
+		if(flightlist.size()==0)	{		
+			flightlist.add(trip);
+			return;
 		}
-		if(List_flights.size() < 6 && List_flights.get(0) != added_flight) {
-			List_flights.add(added_flight);
-			for (int a = 0; a < List_flights.size(); a++) {
-				Date firstDate = List_flights.get(a).getDeparture();
-				
-				for (int b = List_flights.size() - 1; b > a; b--) {
-		        	  Date lastDate = List_flights.get(b).getDeparture();
-		              if (firstDate.after(lastDate)) {
-		                  Flight tmp = List_flights.get(a);
-		                  List_flights.set(a,List_flights.get(b)) ;
-		                  List_flights.set(b,tmp);
-		              }
-		        }
-		   }	
-		}
-	}
-	public long getTotalLayover() {
-		long layover_dur = 0;
-		if(List_flights == null) return 0;
-
-		if(List_flights.size() > 1) {
-			long F_Arrival = 0;
-			long  N_Departure = 0;
-			
-			for (int a = 0; a < List_flights.size(); a++) {
-				F_Arrival = List_flights.get(a).getArrival().getTime();  		
-				if(a+1 < List_flights.size()) {	
-					N_Departure = List_flights.get(a+1).getDeparture().getTime();		
-					layover_dur = layover_dur + ((N_Departure-F_Arrival)/60000);
+		
+		else {
+			for (int i = 0; i < flightlist.size(); i++) {
+				if(trip.isBefore(flightlist.get(0))) {
+					flightlist.add(0,trip);
+					return;
 				}
-			}return layover_dur;	
-				
-		}return 0;
+				if(flightlist.get(flightlist.size()-1).isBefore(trip)) {
+					flightlist.add(trip);
+					return;
+				}
+				if(flightlist.size()>1) {
+					if(!(flightlist.get(i).overlapsWith(trip))) {
+					if(!(trip.overlapsWith(flightlist.get(i+1)))) {
+						flightlist.add(i+1,trip);
+						return;
+					}
+				}
+				}
+			}	
+		}
 	}
-
+	
+	
+	
+	public String toString() {
+		String temp="";
+		String[] strArr = new String[flightlist.size()];
+		for(int i=0;i<flightlist.size();i++) {
+				if(i<6) {
+				temp=+i+"\t"+flightlist.get(i).getStart().toString()+"\t"+flightlist.get(i).getEnd().toString()+"\n"; 
+				strArr[i]=temp;
+				}
+			
+		}
+		 StringBuffer sb = new StringBuffer();
+	      for(int i = 0; i < strArr.length; i++) {
+	         sb.append(strArr[i]);
+	      }
+		temp = sb.toString();
+		System.out.println(name+"\n"+temp);
+		return name+"\n"+temp;
+		
+	}
+	
 }
